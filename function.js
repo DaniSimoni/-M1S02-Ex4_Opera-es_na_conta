a6fd71d6c8aef7d587c38d75f006682ba0985d00
Daniel Simoni Wegher <w.daniels@hotmail.com>
2023-02-14 14:01:36 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,44 @@
+
+localStorage.setItem('username', 'Walberson');
+
+let username = localStorage.getItem('username');
+console.log(username);
+
+localStorage.removeItem('username');
+console.log(username);
+
+localStorage.setItem('username', 'Daniel');
+console.log(username);
+
+
+
+localStorage.setItem('email', 'walberson@walberson.com.br');
+
+let useremail = localStorage.getItem('email');
+console.log(useremail);
+
+localStorage.removeItem('email');
+console.log(useremail);
+
+localStorage.setItem('useremail', 'daniel@daniel.com.br');
+console.log(useremail);
+
+
+
+localStorage.setItem('url', 'https://drive.google.com/file/d/1c4646VsaXL1F2Iz1tFKP_vtfZjWM6JB9/view');
+
+let userurl = localStorage.getItem('url');
+console.log(userurl);
+
+localStorage.removeItem('email');
+console.log(userurl);
+
+localStorage.setItem('useremail', 'https://trello.com/b/21TA6Qa7/daniel-simoni-wregher');
+console.log(userurl);
+
+
+
+
+
+
+
